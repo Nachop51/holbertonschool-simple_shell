d55cdd581d93b6e3e6e09a221821b5ea3f4a9a5f
Nachop51 <nachoperalta0@gmail.com>
2022-04-05 00:43:20 +0000

    path function added

---
+++ n/path.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+
+char *_status(char *PATH, char *filename);
+char *str_concat(char *s1, char *s2);
+
+int main(int ac, char **av, char **ep)
+{
+	char *PATH = getenv("PATH"), *r;
+
+	if (ac != 2)
+		return (1);
+	r = _status(PATH, av[1]);
+	if(r != NULL)
+		printf("%s\n", r);
+	return (0);
+}
+
+char *_status(char *PATH, char *filename)
+{
+	char *cpy = strdup(PATH);
+	char *token, *absolute;
+	struct stat st;
+
+	token = strtok(cpy, ":");
+	filename = str_concat("/", filename);
+	while (token != NULL)
+	{
+		absolute = str_concat(token, filename);
+		if (stat(absolute, &st) == 0)
+			return (absolute);
+		token = strtok(NULL, ":");
+	}
+	return (NULL);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, n;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	do {
+		i++;
+	} while (s1[i - 1]);
+	do {
+		j++;
+	} while (s2[j - 1]);
+	str = malloc(sizeof(char) * (i + j - 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (n = 0; n < i; n++)
+		str[n] = s1[n];
+	for (n = 0; n < j; n++)
+		str[n + i - 1] = s2[n];
+	return (str);
+}
