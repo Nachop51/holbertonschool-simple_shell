3e452aefa202568730823d86b3ce02726eaefe9c
Nachop51 <nachoperalta0@gmail.com>
2022-04-07 15:58:02 +0000

    Fixed only one space and command case

--- n/hsh.c
+++ n/hsh.c
@@ -61,6 +61,10 @@ int _checkChars(char *str)
 		if (str[i] != 32 && str[i] != 10)
 		{
 			r = 0;
+			if(str[0] == ' ' && str[1] != ' ')
+			{
+				str = strtok(str, " ");
+			}
 			break;
 		}
 		i++;
@@ -89,14 +93,17 @@ int args(char *str)
 {
 	int i = 1, counter = 0;
 
-	if(str[0] != ' ')
+	if (str[0] != ' ')
 			counter++;
+	if (str[0] == ' ' && (str[1] != ' ' && str[1] != '\0'))
+		counter++;
 	while (str[i])
 	{
 		if (str[i] == ' ' && (str[i + 1] != ' ' && str[i + 1] != '\0'))
 			counter++;
 		i++;
 	}
+	printf("%d\n", counter);
 	return (counter);
 }
 
