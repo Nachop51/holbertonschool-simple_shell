a97689d8444a124df3c3b61aa4581737fe6767ae
KevinFRX <xerokf13@gmail.com>
2022-04-07 06:37:56 -0700

    update

--- k/main.h
+++ k/main.h
@@ -26,21 +26,23 @@ typedef struct list_s
 
 /* MAIN FUNCTIONS */
 
-char **tokenize(char *str);
+char **tokenize(char *str, int builtIn);
 char *_getenv(const char *name);
 int _checkChars(char *str);
-int _checkExit(char *str);
-char *_status(char *PATH, char *filename);
+int _checkBuiltIn(char *str);
+char *_status(char *filename);
 int args(char *str);
 void sig_handler(int signo);
 void free_array(char **array);
 void free_and_exit(char *buffer);
 int _setenv(char *name, char *value, int overwrite);
 char *create_variable(char *name, char *value);
+void printenv(void);
+
 /* STRINGS */
 
 char *_strcpy(char *dest, char *src);
-int _strcmp(const char *s1, char *s2);
+int _strcmp(const char *s1, const char *s2);
 char *_strdup(const char *str);
 int _strlen(const char *s);
 char *str_concat(char *s1, char *s2);
--- k/path.c
+++ k/path.c
@@ -1,40 +1,36 @@
 #include "main.h"
 
 /**
- * path - searches a route of a file
- * @name: name of the file
- *
- * Return: Absolute route of a file or NULL
- */
-char *path(char *name)
-{
-	char *PATH = getenv("PATH"), *r = _status(PATH, name);
-
-	if (r == NULL)
-		return (NULL);
-	return (r);
-}
-/**
  * _status - checks if a file exists
- * @PATH: Enviroment variable PATH
+ * @PATH: Environment variable PATH
  * @filename: name of the file
  *
  * Return: Absolute route of a file or NULL
  */
-char *_status(char *PATH, char *filename)
+char *_status(char *filename)
 {
-	char *cpy = strdup(PATH);
+	char *PATH = _getenv("PATH");
+	char *cpy = _strdup(PATH), *concatenated;
 	char *token, *absolute;
 	struct stat st;
 
 	token = strtok(cpy, ":");
-	filename = str_concat("/", filename);
+	concatenated = str_concat("/", filename);
 	while (token != NULL)
 	{
-		absolute = str_concat(token, filename);
+		absolute = str_concat(token, concatenated);
 		if (stat(absolute, &st) == 0)
+		{
+			free(PATH);
+			free(cpy);
+			free(concatenated);
 			return (absolute);
+		}
 		token = strtok(NULL, ":");
+		free(absolute);
 	}
+	free(PATH);
+	free(concatenated);
+	free(cpy);
 	return (NULL);
 }
